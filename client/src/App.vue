5deda45a8ef38d2d16e85d6f4de784c53271cf19
Thatcher Deyoe <tdeyoe2@gmail.com>
2021-10-07 15:18:43 -0400

    messing with vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container">
+      <Nav />\
+      <router-view/>
   </div>
-  <router-view/>
+
 </template>
 
 <style lang="scss">
 
 
 </style>
+
+<script>
+    
+    import Nav from "./components/Nav.vue";
+
+    export default{
+      components: {
+        Nav
+      }
+    }
+
+
+</script>
---
+++ client/src/views/Feed.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="section">
+    <h1 class="tile">
+        Feed page
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
