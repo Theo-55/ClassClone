90fa9eb4a209935939f939e40c11fdd45e0c07f4
Thatcher Deyoe <tdeyoe2@gmail.com>
2021-10-19 19:08:13 -0400

    end of class

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="section">
-      <h1 class="title">
-          Feed Page
-
-
-      </h1>
+      <h1 class="title"> Feed Page </h1>
+      
       <div class="columns">
         <div class="column is-one-third is-offset-one-third">
             
@@ -25,7 +22,9 @@ import Post from '../components/Post.vue';
 import session from "../services/session";
 import { GetWall } from "../services/posts";
 export default {
-  components: { Post },
+    components: {
+        Post
+    },
     data: ()=> ({
         posts: GetWall(session.user.handle)
     })
