a724f675895aab85a0618aec9fd830a7256ed434
Thatcher Deyoe <tdeyoe2@gmail.com>
2021-10-25 00:13:04 -0400

    creating server

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,11 +1,14 @@
+
+/*  B"H
+*/
 const express = require("express");
+const model = require("../models/users");
 
 const app = express.Router();
 
 app
-
     .get("/", (req, res, next) =>{
-        res.send([{name: "Theo"} ]);
+        res.send(model.GetAll());
     })
 
-    module.exports = app;
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+
+const usersController = require('./controllers/users');
+
 const app = express()
 const port = 3000
 
 app
-  .get('*', (rew, res, next)=>{
-    console.log('A request came');
-  })
-  .get('/', (req,res) => {
-    console.log('Hello World');
-  })
-  .get('/newpaltz', (rew, res)=>{
-    
-  })
+    .use('/', express.static(path.join(__dirname, '../docs')) )
+    .use('/users', usersController )
+
+app
+    .get('*', (req, res) => res.sendFile(path.join(__dirname, '../docs/index.html')) )
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
